c49d1dfaaca5ce7a2177b7a97201f4e4e57f2a56
Jonathan Dcruz <91173453+professeur-hunter@users.noreply.github.com>
2024-02-27 11:43:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image, ImageOps
 #from tensorflow.keras.preprocessing.image import load_img,img_to_array
-from tensorflow.keras.models import load_model
+from keras.models import load_model
 import numpy as np
 import keras
 st.title("THYDet")
@@ -9,7 +9,7 @@ st.header("Ultrasound Thyroid Detector")
 st.text("Upload an Image.....")
 
 from PIL import Image
-from tensorflow.keras.utils import load_img,img_to_array
+from keras.utils import load_img,img_to_array
 import numpy as np
 import keras
 from keras.models import load_model
