6f52189521143f580b330de996f8ec7628e628a0
Jonathan Dcruz <91173453+professeur-hunter@users.noreply.github.com>
2024-02-27 14:22:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import tensorflow
 from PIL import Image, ImageOps
 #from tensorflow.keras.preprocessing.image import load_img,img_to_array
 from tensorflow.keras.models import load_model
