746e2583a9f0f1ab5a394bedc268f7298d377b77
Jonathan Dcruz <91173453+professeur-hunter@users.noreply.github.com>
2024-02-27 11:40:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from img_classification.py import teachable_machine_classification
 from PIL import Image, ImageOps
 from tensorflow.keras.preprocessing.image import load_img,img_to_array
 from tensorflow.keras.models import load_model
@@ -8,6 +7,34 @@ import keras
 st.title("THYDet")
 st.header("Ultrasound Thyroid Detector")
 st.text("Upload an Image.....")
+
+from PIL import Image
+from keras.preprocessing.image import load_img,img_to_array
+import numpy as np
+import keras
+from keras.models import load_model
+from PIL import Image, ImageOps
+def teachable_machine_classification(img, weights_file):
+    
+    model = keras.models.load_model(weights_file)
+
+    
+    data = np.ndarray(shape=(1, 384, 384, 3), dtype=np.float32)
+    image = img
+  
+    size = (384, 384)
+    image = ImageOps.fit(image, size, Image.ANTIALIAS)
+
+  
+    image_array = np.asarray(image)
+  
+    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+
+  
+    data[0] = normalized_image_array
+
+    prediction = model.predict(data)
+    return np.argmax(prediction)
      
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg","png","jpeg"])
 if uploaded_file is not None:
